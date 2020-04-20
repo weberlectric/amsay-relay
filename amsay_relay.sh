455a77c59d91e0e03853d62541eb4656d18534b2
David Weber <djw612@gmail.com>
2020-04-20 17:35:20 -0400

    sanitize inputs

--- amsay_relay.sh
+++ amsay_relay.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This script reads in lines from the server log produced by the /amsay command.
+# This script reads in lines produced by the /amsay command from the server log
 # The player's name and the message content are parsed out and then sent to
 # Discord via a webhook.
 
@@ -42,7 +42,7 @@ send_to_discord() {
 # Redirect tail output of server log to a read-while loop, sending the input
 # to Discord
 watch_amsay() {
-    while read line; do
+    while read -r line; do
         # Grab the raw player name, including color codes
         raw_player_name=$(
             echo "$line" | \
@@ -53,7 +53,10 @@ watch_amsay() {
         # We want to remove color codes from player name for better readability
         clean_player_name=$(echo $raw_player_name | sed 's/[!^][0-9]//g') 
 
-        message=$(echo "$line" | awk -F"$(printf '%q\n' "$raw_player_name"): " '{print $NF}')
+        message=$(echo "$line" \
+		| awk -F"$(printf '%q\n' "$raw_player_name"): " '{print $NF}' \
+		| sed 's/\\/\\\\/g' # escape slashes
+		)
 
         send_to_discord $1 "$clean_player_name" "$message"
     done < <(tail -fn 0 $2 | grep --line-buffered "say_admin") # /amsay only 
